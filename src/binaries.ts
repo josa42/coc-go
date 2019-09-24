3baed3631bee7d6b8babd932b5d1cb31398b0af5
Josa Gesell <josa@gesell.me>
2019-09-24 17:59:22 +0200

    Revert "Install gopls latest version"
    This reverts commit 9ff4d37f21dbb99f5d9490a99f6a173648f4439b.

--- src/binaries.ts
+++ src/binaries.ts
@@ -1,4 +1,4 @@
-export const GOPLS = 'golang.org/x/tools/cmd/gopls@latest'
+export const GOPLS = 'golang.org/x/tools/cmd/gopls'
 export const GOMODIFYTAGS = 'github.com/fatih/gomodifytags'
 export const GOTESTS = "github.com/cweill/gotests/..."
 export const GOPLAY = "github.com/haya14busa/goplay/cmd/goplay"
